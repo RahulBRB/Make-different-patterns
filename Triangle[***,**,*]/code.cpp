d45be9022851dd186f2390ea7697b5ab2e5c8afd
Rahul Baskey <86495244+RahulBRB@users.noreply.github.com>
2022-09-16 21:45:01 +0530

    Update code.cpp

--- Triangle[***,**,*]/code.cpp
+++ Triangle[***,**,*]/code.cpp
@@ -5,12 +5,11 @@ int main(){
     int n;
     cin>>n;
     
-    for(int i=0;i<n;i++){
-        for(int j=n;j>i;j--){
-            cout<<"* ";
+    for(int i=1;i<=n;i++){
+        for(int j=n; j>=i;j--){
+            cout<< "* ";
         }
         cout<< '\n';
     }
-    
     return 0;
 }
