cb1ae1788b5fc7168e17116f12fddddcea984988
Rahul Baskey <86495244+RahulBRB@users.noreply.github.com>
2022-09-16 20:22:15 +0530

    Update code.cpp

--- Triangle[1,22,333,4444]/code.cpp
+++ Triangle[1,22,333,4444]/code.cpp
@@ -5,11 +5,11 @@ int main(){
     int n;
     cin>>n;
     
-    for(int i=0;i<=n;i++){
+    for(int i=1;i<=n;i++){
         for(int j=1;j<=i;j++){
             cout<< i << " ";
         }
-        cout<< '\n';
+        cout << '\n';
     }
     
     return 0;
